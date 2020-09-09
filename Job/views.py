5c2bfc8c0881d0657921daced50ad95055b8f470
Ammar Yasser <Ammarasc74@gmail.com>
2020-09-09 21:53:35 +0200

    add data to job list & job detail html files

--- Job/views.py
+++ Job/views.py
@@ -6,11 +6,13 @@ from .models import job
 def job_list(request):
     job_list = job.objects.all()
     print(job_list)
-    context = {'jobs': job_list }
+    context = {'jobs': job_list } #template name 
     return render(request,'job/job_list.html',context)
 
 
 
 
-def job_detail(request):
-    pass
+def job_detail(request, id):
+    job_detail = job.objects.get(id=id)
+    context = {'job':job_detail}
+    return render(request,'job/job_detail.html',context)
