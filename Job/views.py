d09168eb1ab94929e94ea491b400b63c42144ced
Ammar Yasser <Ammarasc74@gmail.com>
2020-09-10 14:44:23 +0200

    Using Paginator in a view function

--- Job/views.py
+++ Job/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import job
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def job_list(request):
     job_list = job.objects.all()
-    print(job_list)
-    context = {'jobs': job_list } #template name 
+    paginator = Paginator(job_list, 1) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    
+    context = {'jobs': page_obj } #template name 
     return render(request,'job/job_list.html',context)
 
 
