70bf2ba774af286f8ed78666ff5d69066c270a9f
Ammar Yasser <Ammarasc74@gmail.com>
2020-09-12 18:49:15 +0200

    Add accounts model fields

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect , render
+from .forms import Signupform
+from django.contrib.auth import authenticate , login 
 
 # Create your views here.
+
+def signup(request):
+
+    if request.method =='POST':
+        form = Signupform(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('accounts/profile')
+    else:
+        form = Signupform()
+    return render(request,'registration/signup.html',{'form':form})
