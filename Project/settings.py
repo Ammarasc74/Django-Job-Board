e1f095e33225a640f5bdce9d455ad3b354416a17
Ammar Yasser <Ammarasc74@gmail.com>
2020-09-20 00:07:54 +0200

    add static root

--- Project/settings.py
+++ Project/settings.py
@@ -138,12 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = BASE_DIR / "media"
